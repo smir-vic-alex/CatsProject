ea1274a0093114c49e0fff13fc7bc6077c63a011
SBT-Smirnov-VA <VASmirnov.SBT@sberbank.ru>
2017-06-08 12:17:24 +0300

    testRename

--- src/main/java/MainStarter.java
+++ src/main/java/MainStarter.java
@@ -2,7 +2,7 @@ import vk.connectors.VKConnectorFactory;
 import vk.connectors.VKConnectorType;
 
 /**
- * Стартер
+ * Стартер приложения
  * Created by Smirnov Victor on 30.03.17.
  */
 public class MainStarter {
