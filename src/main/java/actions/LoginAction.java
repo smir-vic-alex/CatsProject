f67b34df0626612bf2efd5106f31467cd9585410
VictorCat <mastersavage@mail.ru>
2017-09-08 00:46:32 +0300

    аутентификация

--- src/main/java/actions/LoginAction.java
+++ src/main/java/actions/LoginAction.java
@@ -1,11 +1,14 @@
 package actions;
 
+import accesses.EncryptUtils;
 import actionForms.LoginActionForm;
+import entities.User;
 import exeptions.access.LoginException;
 import exeptions.access.LoginNotFoundException;
 import hibernate.services.AuthService;
 import entities.Login;
 import entities.Password;
+import hibernate.services.UserService;
 import org.apache.struts.action.*;
 import utils.WebContext;
 
@@ -18,28 +21,30 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginAction extends OperationActionBase {
 
     private static final AuthService service = new AuthService();
+    private static final UserService userService = new UserService();
 
     public ActionForward start(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         LoginActionForm loginActionForm = (LoginActionForm) form;
 
         Login login = getLogin(loginActionForm.getLogin());
-        checkPassword(login, loginActionForm.getPassword());
-        sessionUpdate();
+        User user = userService.getUserByLogin(login);
+        checkPassword(user.getPassword(), loginActionForm.getPassword());
+        sessionUpdate(user);
 
         return mapping.findForward("clientPage");
     }
 
-    private void sessionUpdate() {
+    private void sessionUpdate(User user) {
         WebContext.getCurrentRequest().getSession(false).setAttribute("login", true);
+        WebContext.getCurrentRequest().getSession(false).setAttribute("user", user);
     }
 
-    private void checkPassword(Login login, String inputPassword) throws LoginException {
-        Password password = service.findPasswordByLogin(login);
-//        if (!password.getPassword().equals(EncryptUtils.code(inputPassword))) {
-//            incrementAttemptsCount();
-//            throw new LoginException();
-//        }
+    private void checkPassword(Password password, String inputPassword) throws LoginException {
+        if (!password.getHash().equals(EncryptUtils.code(inputPassword))) {
+            incrementAttemptsCount();
+            throw new LoginException();
+        }
     }
 
     private Login getLogin(String loginName) throws LoginNotFoundException {
--- src/main/java/hibernate/services/AuthService.java
+++ src/main/java/hibernate/services/AuthService.java
@@ -2,19 +2,27 @@ package hibernate.services;
 
 import hibernate.BusinessService;
 import entities.Login;
-import entities.Password;
+import hibernate.HibernateExecutor;
+import org.hibernate.query.Query;
+
+import javax.persistence.NoResultException;
 
 /**
  * Created by SBT-Smirnov-VA on 20.07.2017.
  */
 public class AuthService extends BusinessService {
 
-    public Login findByLogin(String login) {
-        return new Login();
+    public Login findByLogin(final String login) {
+        return new HibernateExecutor<Login>().execute((session) ->
+                {
+                    try {
+                        Query<Login> query = session.createNamedQuery("entities.Login.get.login.by.login", Login.class);
+                        query.setParameter("login", login);
+                        return query.getSingleResult();
+                    } catch (NoResultException e) {
+                        return null;
+                    }
+                }
+        );
     }
-
-    public Password findPasswordByLogin(Login login) {
-        return new Password();
-    }
-
 }
