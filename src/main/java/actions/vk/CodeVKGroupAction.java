1cadb746905c26c2665462b6c937c7d343592484
SBT-Smirnov-VA <VASmirnov.SBT@sberbank.ru>
2017-10-02 12:30:49 +0300

    как так..

--- src/main/java/actions/vk/CodeVKGroupAction.java
+++ src/main/java/actions/vk/CodeVKGroupAction.java
@@ -6,7 +6,7 @@ import networks.vk.connectors.VKConnectorFactory;
 import networks.vk.connectors.VKConnectorType;
 
 /**
- * 50857f5886cb068c36f8f366fd43f92774657d4b7a88fa64020e7ed5ab596ed76aff991a8d1ccb0c65379
+ *
  * Created by Виктор on 30.09.2017.
  */
 public class CodeVKGroupAction extends CodeVKAction {
