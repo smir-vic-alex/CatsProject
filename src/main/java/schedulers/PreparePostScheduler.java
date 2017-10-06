f8add362e2ef99f2ea53dae98a7bf28767d14c37
VictorCat <mastersavage@mail.ru>
2017-10-07 01:01:12 +0300

    Механизм планировщика

---
+++ src/main/java/schedulers/PreparePostScheduler.java
@@ -0,0 +1,15 @@
+package schedulers;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * Created by Виктор on 07.10.2017.
+ */
+public class PreparePostScheduler implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+}
